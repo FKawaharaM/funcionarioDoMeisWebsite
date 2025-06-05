8bcf1a5febafe18687245edb6aec6374658f75aa
FKawaharaM <fkawahara@protonmail.com>
2025-06-05 13:07:28 -0300

    Update repoName

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/funcionariodomeiswebsite/', // your repo name!
+  base: '/funcionarioDoMeisWebsite/', // your GitHub repo name
   plugins: [react()]
 })
