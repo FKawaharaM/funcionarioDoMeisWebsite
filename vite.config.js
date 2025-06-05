8c9b069090a4e29ce315423d83e49bf589236548
FKawaharaM <fkawahara@protonmail.com>
2025-06-05 17:19:42 -0300

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/funcionarioDoMeisWebsite/', // your GitHub repo name
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
